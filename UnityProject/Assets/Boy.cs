a68c945b824285ccae37c5ea791f467b6db749af
bb790619 <54761440+bb790619@users.noreply.github.com>
2019-11-07 16:34:06 +0800

    完成人物跳躍
    1.完成人物跳越(一段跳)及音效
    2.補上錄影結果

--- UnityProject/Assets/Boy.cs
+++ UnityProject/Assets/Boy.cs
@@ -4,16 +4,38 @@ public class Boy : MonoBehaviour
 {
     [Header("移動速度"), Range(0f, 100f)]
     public float speed = 1.5f;
-    [Header("跳躍高度"), Range(100, 1500)]
-    public int jump = 100;
-
+    [Header("跳躍高度"), Range(1, 100)]
+    public int jump = 5;
+    Rigidbody Rb;      //剛體
+    AudioSource Audio; //音效
+    bool IsGround = true;//是否在地面上
     private void Update()
     {
         Move();
+        Rb = GetComponent<Rigidbody>();
+        Audio = GetComponent<AudioSource>();
     }
 
     private void Move()
     {
         transform.Translate(0, 0, speed * Time.deltaTime);
+    }
+    public void Jump()
+    {
+        if (IsGround == true)//在地面上才能跳躍
+        {
+            Rb.AddForce(new Vector3(0, jump, 0), ForceMode.Impulse);
+            Audio.Play();
+            IsGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.name == "地板")
+        {
+            print("地面上");
+            IsGround = true;
+        }
     }
 }
